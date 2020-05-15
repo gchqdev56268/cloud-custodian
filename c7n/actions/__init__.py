2b4b8cfdc5465bea13b431f42115385891e0e50d
gchqdeveloper56268 <60973232+gchqdev56268@users.noreply.github.com>
2020-05-15 14:06:59 +0000

    Added auto-tag-role

--- c7n/actions/__init__.py
+++ c7n/actions/__init__.py
@@ -15,9 +15,9 @@
 
 from .core import Action, EventAction, BaseAction, ActionRegistry
 from .autotag import AutoTagUser
+from .autotagrole import AutoTagRole
 from .invoke import LambdaInvoke
 from .metric import PutMetric
 from .network import ModifyVpcSecurityGroupsAction
 from .notify import BaseNotify, Notify
 from .policy import RemovePolicyBase, ModifyPolicyBase
-
